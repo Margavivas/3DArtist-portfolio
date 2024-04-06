ab8cc55e327292bacb2fb03fab87d6c43ad7a5d3
Margavivas <marga.imdesigner@gmail.com>
2024-04-06 20:27:25 +0200

    feat: menu bar

---
+++ src/components/ViewContainer/viewContainer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="viewContainer centerContainer-v">
+        <NavBar class="navBar"></NavBar>
+    </div>
+</template>
+
+<script setup>
+import NavBar from '../menuBar/NavBar.vue';
+</script>
+
+<style scoped>
+.viewContainer{
+width: 100%;
+height: 100%;
+margin: 0px;
+padding: 0px;
+/* border: solid blue 1px; */
+}
+.navBar{
+    width: 50%;
+    position: fixed;
+    /* border: solid red 1px; */
+}
+
+
+</style>
---
+++ src/components/menuBar/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="navBarContainer centerContainer-v centerContainer-h borderContainer">
+        <ul>
+            <li class="bold">About Me</li>
+            <li><img src="./img/logo.svg" alt="Manez Logo"></li>
+            <li class="bold">Contact</li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+@import '../../style.css';
+.navBarContainer{
+    width: 480px;
+    height: 60px;
+    background-image: url(./img/navBar-bg.svg);
+    background-size: contain;
+    background-repeat: no-repeat;
+    /* border: solid blue 1px; */
+}
+
+
+</style>
